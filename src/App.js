34eb1651b8ee8ad66bf55f62341dcd497c53bdb0
FilipRich <filip.richeberg@hotmail.com>
2022-05-17 20:38:39 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
+
 
 function App() {
+  const axios = Axios;
+
+  const [fact, setFact] = useState();
+
+  function getFact() {
+    axios.get("https://asli-fun-fact-api.herokuapp.com/")
+      .then(function (response) {
+        console.log(response);
+        setFact({
+          id: response.data.id,
+          fact: response.data.fact
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
   return (
     <div className="App">
-      <p>hej</p>
+      <button onClick={getFact}>Klicka för att få random fakta</button>
     </div>
+    
   );
 }
 
